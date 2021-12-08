6ff44205131edec095d6fcf4cb0e67f2a6da9ae1
Nicole Quijada <quijadabnicolea@gmail.com>
2021-12-08 16:00:18 -0300

    chore(footer):add css and icons

--- src/utils/dataCarousel.ts
+++ src/utils/dataCarousel.ts
@@ -2,7 +2,7 @@ export const dataCarousel = [
     {
         "id":1,
         "name":"iPhone 13 pro",
-        "description":"pantalla6.0 color gold rose" ,
+        "description":"256GB 6.1 pulgadas color gold rose" ,
         "price":1200000,
         "active":true,
         "img":"https://i.postimg.cc/bNDkwdXD/3.png"
@@ -10,15 +10,15 @@ export const dataCarousel = [
     {
         "id":2,
         "name":"Audifonos Beats",
-        "description":"modelo 3" ,
+        "description":"Solo 3 Over Ear inalambrico Rose Gold" ,
         "price":1200000,
         "active":false,
         "img":"https://i.postimg.cc/T1Sbcvb3/5.png"
     },
     {
         "id":3,
-        "name":"Macbook pro",
-        "description":"M1" ,
+        "name":"MacBook pro",
+        "description":"MacBook Pro 13 M1 8GB RAM 256GB SSD Gold Touch Bar" ,
         "price":1200000,
         "active":false,
         "img":"https://i.postimg.cc/8knLGMYg/2.png"
@@ -26,7 +26,7 @@ export const dataCarousel = [
     {
         "id":4,
         "name":"Samsung Galaxy",
-        "description":"pantalla6.0" ,
+        "description":"Smartphone Galaxy Note 20 256GB" ,
         "price":1200000,
         "active":false,
         "img":"https://i.postimg.cc/dtbdkvvW/4.png"
